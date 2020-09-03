9af55f1879137e23c04b9ee5ca2c397321039f7d
shoaib <shoaibakhtar1912981@gmail.com>
2020-09-03 11:46:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Person from './person/person';
+z
+const app = props =>  {
+   useState()
+{
+    return (
+      <div className="App">
+        <h1>Hi I am react app</h1>
+        <button onCli ck={this.switchNameHandler}>switch name</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}>My Hobbies:playing</Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
+        <p>This is really working</p>
+        </div>
+    );
+  }
+}
+event.target.value
+export default app;
+
+state = {
+  person: [
+    {name:"Max",age:50},
+    {name:"Manu",age:56},
+    {name:"shoaib",age:15}
+
+  ]
+}
+
+switchNameHandler = () => {
+  // console.log('it was clicked')
+  this.setState({
+    person: [
+      {name:"Maxmillian",age:50},
+      {name:"Manu",age:56},
+      {name:"shoaibakhtar",age:15}
+
+    ]
+  })
+}
